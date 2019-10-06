ab978718bbdc9b0e953b168d5630e33ea9717ed6
13127975951@163.com <zaiya128339>
2019-10-06 17:34:40 +0800

    引入antd组件和事件传参

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import {Button,message} from 'antd'
+import './assets/App.css';
 
 function App() {
+  const handleClick=(event,data)=>{
+    console.log(data)
+    message.success('成功了。。。。')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p> 黎在亚</p>
+        <Button type="primary" onClick={(event)=>{handleClick(event,123)}}> 按钮</Button>
       </header>
     </div>
   );
